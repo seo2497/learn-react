5a9856c23e2b32855990b41f68add8f46a99c333
seo2497 <seo2497@gmail.com>
2026-09-16 16:21:34 +0900

    Feat:props

--- src/App.js
+++ src/App.js
@@ -7,35 +7,54 @@ function App() {
   // 기존 코딩은 각각 할당
   const animals = ["dog", "cat", "pig"];
   // const second = animals[1]
-  
-  const [first, second] = animals
 
-  console.log("🚀 ~ App ~ first:", first)
-  console.log("🚀 ~ App ~ second:", second)
+  const [first, second] = animals;
+
+  const handleButtonClick = (data) => {
+    console.log("click", data)
+  }
+
+  console.log("🚀 ~ App ~ first:", first);
+  console.log("🚀 ~ App ~ second:", second);
   // view
   return (
     <div className="App">
       변경~ !!
-
       {/* <Greeting />
       <Greeting /> */}
-      <Card imgUrl={"https://cf.image-farm.s.zigzag.kr/original/cms/2026/09/10/202609100241225840_031442.jpg?width=1029&height=1188&quality=80&format=webp&transparent=true"} itemName={"모델1"}/>
-      <Card imgUrl={"https://cf.image-farm.s.zigzag.kr/original/cms/2026/09/06/202609061348032352_092237.png?quality=80&format=webp&transparent=true&width=1029&height=1188"} itemName={"모델2"}/>
-      <Card imgUrl={"https://cf.product-image.s.zigzag.kr/original/d/2026/8/24/2833_202608241452277531_95137.gif?width=400&height=400&quality=80&format=webp&transparent=true"} itemName={"모델3"}/>
-      <Card />
-      <Card />
+      <Card
+        imgUrl={
+          "https://cf.image-farm.s.zigzag.kr/original/cms/2026/09/10/202609100241225840_031442.jpg?width=1029&height=1188&quality=80&format=webp&transparent=true"
+        }
+        itemName={"모델1"}
+        onButtonClicks={handleButtonClick}
+      />
+      <Card
+        imgUrl={
+          "https://cf.image-farm.s.zigzag.kr/original/cms/2026/09/06/202609061348032352_092237.png?quality=80&format=webp&transparent=true&width=1029&height=1188"
+        }
+        itemName={"모델2"}
+        onButtonClicks={handleButtonClick}
+      />
+      <Card
+        imgUrl={
+          "https://cf.product-image.s.zigzag.kr/original/d/2026/8/24/2833_202608241452277531_95137.gif?width=400&height=400&quality=80&format=webp&transparent=true"
+        }
+        itemName={"모델3"}
+        onButtonClicks={handleButtonClick}
+      />
     </div>
   );
 }
 
 const todoItem = {
-todo : "React학습하기",
-isComplete : false
-}
+  todo: "React학습하기",
+  isComplete: false,
+};
 
-const {todo:todoname, isComplete } = todoItem
- console.log("🚀 ~ isComplete:", isComplete)
- console.log("🚀 ~ todoname:", todoname)
- console.log("🚀 ~ todo:", todoname)
- 
+const { todo: todoname, isComplete } = todoItem;
+console.log("🚀 ~ isComplete:", isComplete);
+console.log("🚀 ~ todoname:", todoname);
+console.log("🚀 ~ todo:", todoname);
+
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-function Card({imgUrl, itemName}) {
+const Card = ({ imgUrl, itemName, onButtonClicks }) => {
   return (
-    <div className='card'>
-      <img src={imgUrl}  alt={itemName}
-      />
+    <div className="card">
+      <img src={imgUrl} alt={itemName} />
       <p>{itemName}</p>
+      <button type="button" onClick={()=>onButtonClicks(itemName)}>제품 페이지로 바로가기</button>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
